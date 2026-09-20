2db77a113adeeae058d1d47ce9ef848954834159
alina-babayan <alina.babayan2@edu.ysu.am>
2026-09-20 17:55:23 +0400

    Delete build directory

--- build/Desktop_Qt_6_9_2_MSVC2022_64bit-Debug/.qt/bin/qt_setup_tool_path.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=C:\Users\USER\Desktop\Homework_QT\6.9.2\msvc2022_64\bin;%PATH%
-%*
--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/ChessTournamentApp_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "ChessTournamentApp")
-set(working_dir "C:/Users/USER/Desktop/ChessTournamentSystem (2)/ChessTournament")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/USER/Desktop/ChessTournamentSystem (2)/ChessTournament/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/ChessTournamentApp_res.txt")
